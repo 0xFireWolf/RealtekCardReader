ee98971f291c4a4079b405e2d33c73a7ed635840
FireWolf <austere.j@gmail.com>
2021-07-12 17:50:55 -0700

    Project: Add the matching information for RTS5129, RTS5139, and RTS5179.

---
+++ RealtekPCIeCardReader/RealtekPCIRegisters.hpp
@@ -0,0 +1,13 @@
+//
+//  RealtekPCIRegisters.hpp
+//  RealtekPCIeCardReader
+//
+//  Created by FireWolf on 7/11/21.
+//
+
+#ifndef RealtekPCIRegisters_hpp
+#define RealtekPCIRegisters_hpp
+
+
+
+#endif /* RealtekPCIRegisters_hpp */
