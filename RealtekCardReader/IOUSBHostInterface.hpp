d6c491d2e5924d1139deb615cea4d117b014956f
FireWolf <austere.j@gmail.com>
2021-08-17 16:18:39 -0700

    Definitions: Remove deprecated functions from `IOUSBHostInterface`.

--- RealtekCardReader/IOUSBHostInterface.hpp
+++ RealtekCardReader/IOUSBHostInterface.hpp
@@ -27,26 +27,6 @@
 ///
 IOUSBHostPipe* IOUSBHostInterfaceFindPipe(IOUSBHostInterface* interface, UInt8 type, UInt8 direction);
 
-/// Type of an action that manipulates with the given intermediate I/O buffer under a user-defined context
-/// The action routine does not need to release the given intermediate buffer
-using IOUSBHostInterfaceBufferAction = IOReturn (*)(IOUSBHostInterface*, IOMemoryDescriptor*, const void*);
-
-///
-/// Perform a custom action with an intermediate buffer that is suitable for I/O transfer
-///
-/// @param interface The host interface
-/// @param direction Specify the direction of the intermediate buffer
-/// @param capacity Specify the capacity of the intermediate buffer
-/// @param action A non-null action that manipulates the intermediate buffer under the given `context`
-/// @param context A nullable user-defined context to be passed to the given `action`
-/// @return `kIOReturnBadArgument` if the given action is null,
-///         `kIOReturnNoMemory` if failed to allocate the intermediate buffer,
-///         otherwise the return value of the given action routine.
-/// @note This function uses `IOUSBHostInterface::createIOBuffer()` to allocate the intermediate buffer.
-///
-DEPRECATE("Replaced by IOUSBHostInterfaceWithIntermediateIOBuffer().")
-IOReturn IOUSBHostInterfaceWithIntermediateIOBuffer(IOUSBHostInterface* interface, IODirection direction, IOByteCount capacity, IOUSBHostInterfaceBufferAction action, const void* context = nullptr);
-
 ///
 /// Perform a custom action with an intermediate buffer that is suitable for I/O transfer
 ///
