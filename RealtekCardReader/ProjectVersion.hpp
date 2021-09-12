6e68dd195c991317d5969aa1649fb5b420d90279
FireWolf <austere.j@gmail.com>
2021-09-11 22:59:02 -0700

    Git: Ignore the project version header.

--- RealtekCardReader/ProjectVersion.hpp
+++ RealtekCardReader/ProjectVersion.hpp
@@ -9,7 +9,7 @@
 #define ProjectVersion_h
 
 #define VERSION "0.9.7"
-#define HASH "0b1a4c6"
-#define TIME "Sun Sep 12 05:00:44 UTC 2021"
+#define HASH "ac9d59e"
+#define TIME "Sun Sep 12 21:02:52 UTC 2021"
 
 #endif /* ProjectVersion_h */
